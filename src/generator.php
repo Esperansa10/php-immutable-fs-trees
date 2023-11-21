3d276ba6fe786a182a96d0ce20b81d5d2f767119
имя фамилия <esperansa10@mail.ru>
2023-11-21 10:12:37 +0300

    questrion about theory

--- src/generator.php
+++ src/generator.php
@@ -4,24 +4,82 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use function Php\Immutable\Fs\Trees\trees\mkdir;
 use function Php\Immutable\Fs\Trees\trees\mkfile;
+use function Php\Immutable\Fs\Trees\trees\getChildren;
+use function Php\Immutable\Fs\Trees\trees\getName;
+use function Php\Immutable\Fs\Trees\trees\getMeta;
+use function Php\Immutable\Fs\Trees\trees\isFile;
+use function Php\Immutable\Fs\Trees\trees\isDirectory;
 
-// BEGIN (write your solution here)
-function generator() {
-    return 
-    mkdir('php-package', [
-        mkfile('Makefile'),
-        mkfile('README.md'),
-        mkdir('dist', []),
-        mkdir('tests',  mkfile('test.php', ['type' => 'text/php'])),
-        mkdir('src',  mkfile('index.php', ['type' => 'text/php'])),
-        mkfile('phpunit.xml', ['type' => 'text/xml']),
-        mkdir('assets',  
-            mkdir('util', mkdir('cli', 
-            mkfile('LICENSE'))), 
-            ['owner' => 'root', 'hidden' => false])
-    ], ['key' => 'value'] );
-        }
+$tree = mkdir('/', [
+    mkdir('eTc', [
+        mkdir('NgiNx'),
+        mkdir('CONSUL', [
+            mkfile('Сonfig.JSON'),
+        ]),
+    ]),
+    mkfile('hOsts'),
+]);
 
-        generator(); 
+function dfs($tree) {
+  echo strtolower(getName($tree)) . "\n";
+   if (isFile($tree)) {
+      return;
+  }
+  $children = getChildren($tree);
+   array_map(fn($child) => dfs($child), $children);
+}
+var_dump(dfs($tree));
 
-// END
+
+
+
+function downcaseFileNames($tree) {
+    $children = getChildren($tree); 
+    
+    $newChildren = array_map(function($child) {
+    $name = strtolower(getName($child));
+    if (isDirectory($child)) {
+    
+        array_map(fn($child) => dfs($child), $children);
+    
+        return mkdir($name, getChildren($child), getMeta($child));
+    }
+    return mkfile($name, getMeta($child));
+    },
+        $children);
+        
+            return mkdir(getName($tree), $newChildren, getMeta($tree));
+
+}
+// var_dump(downcaseFileNames($tree));
+
+
+
+// downcaseFileNames($tree);
+
+// ---
+// $tree = mkdir(
+//     'my documents', [
+//         mkfile('avatar.jpg', ['size' => 100]),
+//         mkfile('passport.jpg', ['size' => 200]),
+//         mkfile('family.jpg',  ['size' => 150]),
+//         mkfile('addresses',  ['size' => 125]),
+//         mkdir('presentations')
+//         ],
+//     );
+
+// $newTree = compressImages($tree);
+
+// function compressImages($tree) {
+//     $children = getChildren($tree); //получаем файлы 
+
+//     $children = array_map(function($child) {
+//         if (isFile($child) and strpos($child['name'], '.jpg')) {
+//         $child['meta']['size'] = $child['meta']['size']/2; }
+//         return $child; }, 
+//         $children);
+
+//     return mkdir(getName($tree), $children, getMeta($tree));
+//     }
+
+// print_r($newTree); 
